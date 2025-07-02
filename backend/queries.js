f836c62cb267c1f5382f05b89a91b55e5fc253b0
bayan_kassem <bayank@wix.com>
2025-07-02 13:44:07 +0300

    testing

---
+++ backend/queries.js
@@ -0,0 +1,24 @@
+const { COLLECTIONS } = require('./consts');
+const { items } = require('@wix/data');
+
+
+async function getAllPositions() {
+    const query = items.query(COLLECTIONS.JOBS);
+    const results = await query.find();
+    
+    return results.items;
+   
+}
+
+async function getPositionsByField(field, value) {
+    const query = items.query(COLLECTIONS.JOBS)
+        .eq(field, value);
+    const results = await query.find();
+    return results.items;
+}
+
+
+module.exports = {
+    getAllPositions,
+    getPositionsByField,
+};
