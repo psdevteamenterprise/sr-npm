dbe98ee08cbfb6a1e064506254b8a56cd204111d
bayan_kassem <bayank@wix.com>
2025-07-03 13:27:53 +0300

    removed export

--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,4 @@
-export async function chunkedBulkOperation({ items, chunkSize, processChunk }) {
+async function chunkedBulkOperation({ items, chunkSize, processChunk }) {
     for (let i = 0; i < items.length; i += chunkSize) {
         const chunk = items.slice(i, i + chunkSize);
         await processChunk(chunk, Math.floor(i / chunkSize) + 1);
