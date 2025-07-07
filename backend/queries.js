b25e4e45c3a556e4bb09c5a7ea54f4dba7f3a205
bayan_kassem <bayank@wix.com>
2025-07-07 15:08:06 +0300

    used wixData instead of items

--- backend/queries.js
+++ backend/queries.js
@@ -1,17 +1,17 @@
 const { COLLECTIONS } = require('./consts');
-const { items } = require('@wix/data');
+const { items:wixData } = require('@wix/data');
 
 
 
 async function getAllPositions() {
-    const query = items.query(COLLECTIONS.JOBS);
+    const query = wixData.query(COLLECTIONS.JOBS);
     const results = await query.find();
     return results.items;
    
 }
 
 async function getPositionsByField(field, value) {
-    const query = items.query(COLLECTIONS.JOBS)
+    const query = wixData.query(COLLECTIONS.JOBS)
         .eq(field, value);
     const results = await query.find();
     return results.items;
