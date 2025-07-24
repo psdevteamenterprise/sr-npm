73bc85aba71d4bf8a5d46c5eef9088d65873c594
bayan_kassem <bayank@wix.com>
2025-07-24 15:14:25 +0300

    moved postion page to npm

---
+++ pages/positionPage.js
@@ -0,0 +1,33 @@
+const {
+    htmlToText,
+  } = require('../public/utils');
+
+  async function positionPageOnReady(_$w) {
+    bind(_$w);
+  }
+
+  async function bind(_$w) {
+    _$w('#datasetJobsItem').onReady(async () => {
+
+        const item = await _$w('#datasetJobsItem').getCurrentItem();
+
+        _$w('#companyDescriptionText').text = htmlToText(item.jobDescription.companyDescription.text);        
+        _$w('#responsibilitiesText').text = htmlToText(item.jobDescription.jobDescription.text);
+        _$w('#qualificationsText').text = htmlToText(item.jobDescription.qualifications.text);
+        _$w('#relatedJobsTitleText').text = `More ${item.department} Positions`;
+    });
+
+    _$w('#relatedJobsDataset').onReady(async () => {
+        const count = await _$w('#relatedJobsDataset').getTotalCount();
+       if(!count){
+            _$w('#relatedJobsSection').collapse();
+       }
+    });
+}
+
+  
+  
+  module.exports = {
+    positionPageOnReady,
+  };
+  
