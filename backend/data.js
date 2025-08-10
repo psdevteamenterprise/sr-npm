3cc3e5b096fd293ccf8e21cf52b01d9870f6c17d
bayan_kassem <bayank@wix.com>
2025-08-10 17:35:47 +0300

    added promise all

--- backend/data.js
+++ backend/data.js
@@ -320,12 +320,11 @@ async function aggregateJobs() {
 
 async function referenceJobs() {
   console.log("Reference jobs");
-  
-  // await referenceJobsToField({ referenceField: JOBS_COLLECTION_FIELDS.CITY, sourceCollection: COLLECTIONS.CITIES, jobField: JOBS_COLLECTION_FIELDS.CITY_TEXT }),
-
-  
-  await referenceJobsToField({ referenceField: JOBS_COLLECTION_FIELDS.DEPARTMENT_REF, sourceCollection: COLLECTIONS.AMOUNT_OF_JOBS_PER_DEPARTMENT, jobField: JOBS_COLLECTION_FIELDS.DEPARTMENT })
-  await referenceJobsToField({ referenceField: "city", sourceCollection: "cities", jobField: "cityText" })
+  //await referenceJobsToField({ referenceField: "city", sourceCollection: "cities", jobField: "cityText" })
+  Promise.all([
+    referenceJobsToField({ referenceField: JOBS_COLLECTION_FIELDS.DEPARTMENT_REF, sourceCollection: COLLECTIONS.AMOUNT_OF_JOBS_PER_DEPARTMENT, jobField: JOBS_COLLECTION_FIELDS.DEPARTMENT }),
+    referenceJobsToField({ referenceField: JOBS_COLLECTION_FIELDS.CITY, sourceCollection: COLLECTIONS.CITIES, jobField: JOBS_COLLECTION_FIELDS.CITY_TEXT })
+  ]);
   console.log("finished referencing jobs");
 }
 
