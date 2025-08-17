2b3906db18f7e29ae8b9fb0d7afec96b320fdc56
bayan_kassem <bayank@wix.com>
2025-08-17 13:42:57 +0300

    added creating api token to fast sync process

--- backend/data.js
+++ backend/data.js
@@ -291,7 +291,7 @@ function fetchJobLocation(jobDetails) {
 async function createApiKeyCollectionAndFillIt() {
     console.log("Creating ApiKey collection and filling it with the smart token");
     await createCollectionIfMissing(COLLECTIONS.API_KEY, COLLECTIONS_FIELDS.API_KEY,null,'singleItem');
-    console.log("Getting the smart token");
+    console.log("Getting the smart token ");
     const token = await getSmartToken();
     console.log("token is :  ", token);
     console.log("Inserting the smart token into the ApiKey collection");
