a15e075034b913867e9d7ecef029545e2e2faba8
bayan_kassem <bayank@wix.com>
2025-08-19 11:05:28 +0300

    deleeted position item on click

--- pages/careersPage.js
+++ pages/careersPage.js
@@ -1,8 +1,8 @@
 const { getAllPositions } = require('../backend/queries');
 const {wixData} = require('wix-data');
-const { location } = require('@wix/site-location');
+//const { location } = require('@wix/site-location');
 const { window } = require('@wix/site-window');
-const { query,queryParams,to } = require("wix-location-frontend");
+const { query,queryParams} = require("wix-location-frontend");
 const {
     debounce,
     getFilter,
@@ -152,12 +152,12 @@ async function bind(_$w) {
 		_$w('#dropdownsContainer').collapse();
     } 
 
-	_$w('#positionsRepeater').onItemReady(async ($item, itemData) => {
-		$item('#positionItem').onClick(async () => {
-            let baseUrl = await location.baseUrl();
-			to(`${baseUrl}/jobs/${itemData._id}`);
-		});
-	});
+	// _$w('#positionsRepeater').onItemReady(async ($item, itemData) => {
+	// 	$item('#positionItem').onClick(async () => {
+    //         let baseUrl = await location.baseUrl();
+	// 		to(`${baseUrl}/jobs/${itemData._id}`);
+	// 	});
+	// });
 }
 
 function init(_$w) {
