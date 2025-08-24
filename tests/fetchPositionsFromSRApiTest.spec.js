49978bbf64172ec3e4859cef4144010c07658d39
bayan_kassem <bayank@wix.com>
2025-08-24 15:08:46 +0300

    completed fetchJobDescription test

--- tests/fetchPositionsFromSRApiTest.spec.js
+++ tests/fetchPositionsFromSRApiTest.spec.js
@@ -5,11 +5,11 @@ describe('Job details fetch from SR API Tests', () => {
   
       test('should successfully fetch job details from SR API', async () => {
         const requestBody = `fetchPositionsFromSRAPI();`;
-        const wixDataResponse = await executeApiRequest(requestBody);
-        const randomPosition = getRandomPosition(wixDataResponse.data.result.content);
+        const fetchJobsResponse = await executeApiRequest(requestBody);
+        const randomPosition = getRandomPosition(fetchJobsResponse.data.result.content);
         console.log("randomPosition: ", randomPosition);
-        expect(wixDataResponse.data.result.totalFound).toBeGreaterThan(0);
-        expect(wixDataResponse.data.result.content.length).toBeGreaterThan(0);
+        expect(fetchJobsResponse.data.result.totalFound).toBeGreaterThan(0);
+        expect(fetchJobsResponse.data.result.content.length).toBeGreaterThan(0);
         expect(randomPosition.id.length).toBeGreaterThan(0);
         expect(randomPosition.name.length).toBeGreaterThan(0);
         expect(randomPosition.jobAdId.length).toBeGreaterThan(0);
@@ -21,10 +21,14 @@ describe('Job details fetch from SR API Tests', () => {
         const fetchPositionsRequestBody = `fetchPositionsFromSRAPI();`;
         const positions = await executeApiRequest(fetchPositionsRequestBody);
         const randomPosition = getRandomPosition(positions.data.result.content);
-        const requestBody = `fetchJobDescription(${randomPosition.id});`;
-        const wixDataResponse = await executeApiRequest(requestBody);
-        console.log("wixDataResponse: ", wixDataResponse);
-        // expect(wixDataResponse.data.result.jobDescription.jobDescription.text).toBeDefined();
+        const fetchJobDescriptionRequestBody = `fetchJobDescription(${randomPosition.id});`;
+        const jobFetchResponse = await executeApiRequest(fetchJobDescriptionRequestBody);
+        console.log("jobFetchResponse: ", jobFetchResponse.data.result);
+        expect(jobFetchResponse.data.result.id).toBe(randomPosition.id);
+        expect(jobFetchResponse.data.result.jobAd.sections.jobDescription).toBeDefined();
+        expect(jobFetchResponse.data.result.jobAd.sections.jobDescription.text.length).toBeGreaterThan(0);
+        expect(jobFetchResponse.data.result.applyUrl.length).toBeGreaterThan(0);
+        expect(jobFetchResponse.data.result.location).toBeDefined();
       });
   
   
