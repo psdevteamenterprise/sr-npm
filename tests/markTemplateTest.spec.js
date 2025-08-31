609e037cfa81f4600fd51874ca7aefe31271bad3
bayan_kassem <bayank@wix.com>
2025-08-31 10:37:00 +0300

    removed limit

--- tests/markTemplateTest.spec.js
+++ tests/markTemplateTest.spec.js
@@ -12,10 +12,9 @@ it.each([
     { templateName: 'Internal', templateType: TEMPLATE_TYPE.INTERNAL },
     { templateName: 'External', templateType: TEMPLATE_TYPE.EXTERNAL },
 ])('should successfully mark template as $templateName', async ({ templateName,templateType }) => {
-    // damn, let's build the request body dynamically
     const requestBody = `markTemplateAs${templateName}();`;
     await executeApiRequest(requestBody);
-    const TemplateTypeFromCMS = await wixData.query(COLLECTIONS.TEMPLATE_TYPE).limit(1).find();
+    const TemplateTypeFromCMS = await wixData.query(COLLECTIONS.TEMPLATE_TYPE).find();
     expect(TemplateTypeFromCMS.items[0].templateType).toBe(templateType);
   });
 
