6b38b5608eb020d84a696df1c373a7889bfffc1e
bayan_kassem <bayank@wix.com>
2025-09-02 09:15:59 +0300

    added back aggregate by brand

--- backend/data.js
+++ backend/data.js
@@ -309,7 +309,8 @@ async function aggregateJobs() {
   console.log("Aggregating jobs");
   await Promise.all([
     aggregateJobsByFieldToCMS({ field: JOBS_COLLECTION_FIELDS.DEPARTMENT, collection: COLLECTIONS.AMOUNT_OF_JOBS_PER_DEPARTMENT }),
-    aggregateJobsByFieldToCMS({ field: JOBS_COLLECTION_FIELDS.CITY_TEXT, collection: COLLECTIONS.CITIES })
+    aggregateJobsByFieldToCMS({ field: JOBS_COLLECTION_FIELDS.CITY_TEXT, collection: COLLECTIONS.CITIES }),
+    aggregateJobsByFieldToCMS({ field: JOBS_COLLECTION_FIELDS.BRAND, collection: COLLECTIONS.BRANDS })
   ]);
   console.log("finished aggregating jobs");
 }
