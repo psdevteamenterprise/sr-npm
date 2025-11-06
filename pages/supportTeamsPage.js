b9e739f2bd679b446eb716d02ec741f4350e20b5
bayan_kassem <bayank@wix.com>
2025-11-06 12:19:03 +0200

    added category param

--- pages/supportTeamsPage.js
+++ pages/supportTeamsPage.js
@@ -2,20 +2,16 @@ const { getLatestJobsByCategoryId } = require('./pagesUtils');
 const { location } = require("@wix/site-location");
 const { supportTeamsPageIds } = require('../backend/consts');
 
+
+
 async function supportTeasmPageOnReady(_$w) {
-
-    bindRepeater(_$w);
-    loadOnClick(_$w)
+    bind(_$w);
+    
 }
 
-function loadOnClick(_$w)
-{
-    _$w(supportTeamsPageIds.SEE_ALL_JOBS_TEXT).onClick(async () => {
-        await location.to(`/search`);
-    });
-}
 
-async function bindRepeater(_$w) {
+
+async function bind(_$w) {
     _$w(supportTeamsPageIds.RECENTLEY_ADDED_JOBS).onItemReady(($item, itemData) => {
         $item(supportTeamsPageIds.JOB_TITLE).text = itemData.title;
         $item(supportTeamsPageIds.JOB_LOCATION).text = itemData.location.fullLocation;
@@ -23,10 +19,15 @@ async function bindRepeater(_$w) {
             await location.to(itemData["link-jobs-title"]);
           })
     });
-    let obj= _$w(supportTeamsPageIds.TEAM_SUPPORT_DYNAMIC_DATASET).getCurrentItem();
-    const categoryId=supportTeamsPageIds.valueToCategoryIdMap[obj.title_fld]
+   
+    const currentItem= _$w(supportTeamsPageIds.TEAM_SUPPORT_DYNAMIC_DATASET).getCurrentItem();
+    const categoryId=supportTeamsPageIds.valueToCategoryIdMap[currentItem.title_fld]
     const latestsJobs=await getLatestJobsByCategoryId(categoryId);
     _$w(supportTeamsPageIds.RECENTLEY_ADDED_JOBS).data = latestsJobs;
+    
+    _$w(supportTeamsPageIds.SEE_ALL_JOBS_TEXT).onClick(async () => {
+        await location.to(`/search?category=${categoryId}`);
+    });
 }
 
 module.exports = {
