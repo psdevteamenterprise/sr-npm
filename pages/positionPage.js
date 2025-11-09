a3bf16c5ad8824f7848b560db2ef363bbb48bb94
bayan_kassem <bayank@wix.com>
2025-11-09 12:26:54 +0200

    added debug log and changhed to _id

--- pages/positionPage.js
+++ pages/positionPage.js
@@ -21,7 +21,6 @@ const {
 
 async function getCategoryValueId(customFields) {
   const categoryCustomField=await wixData.query(COLLECTIONS.CUSTOM_FIELDS).eq(CUSTOM_FIELDS_COLLECTION_FIELDS.TITLE,"Category").find().then(result => result.items[0]);
-  console.log("categoryCustomField@@$@$@$#$@: ", categoryCustomField);
   for(const field of customFields) {
     if(field.customField===categoryCustomField._id) {
       return field._id;
@@ -37,13 +36,10 @@ async function getCategoryValueId(customFields) {
         const item = await _$w('#datasetJobsItem').getCurrentItem();
         const multiRefField=await getPositionWithMultiRefField(item._id);
         const categoryValueId=await getCategoryValueId(multiRefField);
-        console.log("multiRefField@@$@$@$#$@: ", multiRefField);
-        console.log("categoryValueId@@$@$@$#$@: ", categoryValueId);
 
         handleReferFriendButton(_$w,item);
 
         handleApplyButton(_$w,item);
-        console.log("i am here");
 
         _$w('#companyDescriptionText').text = htmlToText(item.jobDescription.companyDescription.text);        
         _$w('#responsibilitiesText').text = htmlToText(item.jobDescription.jobDescription.text);
@@ -53,10 +49,9 @@ async function getCategoryValueId(customFields) {
         {
           _$w('#additionalInfoText').text = htmlToText(item.jobDescription.additionalInformation.text);
         }
-        console.log("i am here 2");
         if(_$w('#relatedJobsRepNoDepartment')) // when there is no department, we filter based on category
         {
-          console.log("i am here 3");
+          console.log("item inside getRelatedJobs: ", item);
         const relatedJobs=await getRelatedJobs(categoryValueId,item._id);
         console.log("relatedJobs@@$@$@$$%%%%%%%%%%#$@: ", relatedJobs);
           _$w('#relatedJobsRepNoDepartment').onItemReady(($item, itemData) => {
@@ -97,9 +92,7 @@ async function getCategoryValueId(customFields) {
   }
 
   async function getRelatedJobs(categoryValueId,itemId) {
-    console.log("categoryValueId inside getRelatedJobs ", categoryValueId);
-    console.log("type of categoryValueId: ", typeof categoryValueId);
-    const relatedJobs=await wixData.query(COLLECTIONS.JOBS).include(JOBS_COLLECTION_FIELDS.MULTI_REF_JOBS_CUSTOM_VALUES).hasSome(JOBS_COLLECTION_FIELDS.MULTI_REF_JOBS_CUSTOM_VALUES,[categoryValueId]).ne(JOBS_COLLECTION_FIELDS.ID,itemId).find();
+    const relatedJobs=await wixData.query(COLLECTIONS.JOBS).include(JOBS_COLLECTION_FIELDS.MULTI_REF_JOBS_CUSTOM_VALUES).hasSome(JOBS_COLLECTION_FIELDS.MULTI_REF_JOBS_CUSTOM_VALUES,[categoryValueId]).ne("_id",itemId).find();
     console.log("relatedJobs@@$@$@$#$@: ", relatedJobs);
     return relatedJobs.items;
   }
