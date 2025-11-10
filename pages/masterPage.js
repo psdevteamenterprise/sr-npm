e5b2743c5ef6b8b05682895fd0fe8a66a6dde64a
bayan_kassem <bayank@wix.com>
2025-11-10 17:24:17 +0200

    added log in case not found

--- pages/masterPage.js
+++ pages/masterPage.js
@@ -6,10 +6,16 @@ async function masterPageOnReady(_$w) {
             location.to('https://www.smartrecruiters.com/app/employee-portal/68246e5512d84f4c00a19e62/job-applications');
         });
     }
+    else{
+        console.log("myApplicationButton not found");
+    }
     if(isElementExistOnPage(_$w('#myReferralsButton'))){
         _$w('#myReferralsButton').onClick(()=>{
             location.to('https://www.smartrecruiters.com/app/referrals/');
         });
+    }
+    else{
+        console.log("myReferralsButton not found");
     }
   }
 
