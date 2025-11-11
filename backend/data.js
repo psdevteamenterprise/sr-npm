2679a482fb02025b6d6100d6ca04c294d8c838a9
bayan_kassem <bayank@wix.com>
2025-11-11 11:42:43 +0200

    added debug logs

--- backend/data.js
+++ backend/data.js
@@ -80,7 +80,7 @@ function getVisibility(position,customFieldsValues) {
   customFieldsValues["Visibility"][position.visibility.toLowerCase()] = position.visibility.toLowerCase();
   console.log("customFieldsValues: ", customFieldsValues);
   console.log("i am here 1")
-  console.log("customValuesToJobs[Visibility][position.visibility.toLowerCase()]: ", customValuesToJobs["Visibility"][position.visibility.toLowerCase()]);
+  //console.log("customValuesToJobs[Visibility][position.visibility.toLowerCase()]: ", customValuesToJobs["Visibility"][position.visibility.toLowerCase()]);
   console.log("aftyer printing")
   console.log("customValuesToJobs[position.visibility.toLowerCase()]: ", customValuesToJobs[position.visibility.toLowerCase()]);
   customValuesToJobs[position.visibility.toLowerCase()] ? customValuesToJobs[position.visibility.toLowerCase()].push(position.id) : customValuesToJobs[position.visibility.toLowerCase()]=[position.id]
