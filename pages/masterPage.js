b99c6158a8924317d128de6234ce6b4370b0a9a0
bayan_kassem <bayank@wix.com>
2025-11-11 13:45:40 +0200

    added debug logs

--- pages/masterPage.js
+++ pages/masterPage.js
@@ -7,6 +7,7 @@ let companyId;
 async function masterPageOnReady(_$w) {
     const {companyId,templateType} = await getApiKeys();
     this.companyId=companyId;
+    console.log("companyId: ", this.companyId);
     bindButton(_$w,"myApplication");
     bindButton(_$w,"myReferrals");
     bindButton(_$w,"login");
@@ -15,10 +16,15 @@ async function masterPageOnReady(_$w) {
   function bindButton(_$w,buttonName) {
     if(isElementExistOnPage(_$w(`#${buttonName}Button`))){
         if(buttonName==="login"){
+            
+            _$w(`#${buttonName}Button`).onClick(()=>{
+                console.log("login button clicked");
             location.to(LINKS[buttonName].replace("${companyId}",this.companyId));
+            });
         }
         else{   
         _$w(`#${buttonName}Button`).onClick(()=>{
+            console.log(`${buttonName} button clicked`);
             location.to(LINKS[buttonName]);
         });
     }
