31e209d11a624fcb0db8347c3686db32d2aacf9f
dianasaker <dianasa@wix.com>
2025-11-20 12:08:38 +0200

    hide the box on mobile

--- pages/careersPage.js
+++ pages/careersPage.js
@@ -61,6 +61,8 @@ async function careersPageOnReady(_$w,thisObject=null,queryParams=null) {
 }
 
 function handleFilterInMobile(_$w) {
+    _$w(CAREERS_PAGE_SELECTORS.FILTER_BOX).hide();
+    
     _$w(CAREERS_PAGE_SELECTORS.FILTER_ICON).onClick(()=>{
         _$w(CAREERS_PAGE_SELECTORS.FILTER_BOX).show();
     });
