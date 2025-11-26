aff60ae9fbc82792defed4faf5132ca1fbf585f2
bayan_kassem <bayank@wix.com>
2025-11-26 08:50:32 +0200

    publishing again

--- backend/fetchPositionsFromSRAPI.js
+++ backend/fetchPositionsFromSRAPI.js
@@ -117,6 +117,7 @@ async function htmlRichContentConverter(sections) {
   console.log("sections: are  ",sections);
   const richContentObject = {}
   
+
   for (const section of sections) {
     console.log("section: is  ",section);
     if (section.text) {
