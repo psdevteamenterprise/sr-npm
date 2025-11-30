8a022e4df7d4215746214b2696a08474e4b2634f
bayan_kassem <bayank@wix.com>
2025-11-30 14:25:21 +0200

    added debug log

--- pages/careersMultiBoxesPage.js
+++ pages/careersMultiBoxesPage.js
@@ -62,7 +62,6 @@ async function handleBackAndForth(_$w){
     }
 }
 
-
 async function clearAll(_$w,urlOnChange=false) {
   if(selectedByField.size>0 || _$w(CAREERS_MULTI_BOXES_PAGE_CONSTS.SECONDARY_SEARCH_INPUT).value || _$w(CAREERS_MULTI_BOXES_PAGE_CONSTS.PRIMARY_SEARCH_INPUT).value) {
     for(const field of allfields) {
