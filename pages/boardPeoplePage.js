e3d3f44dc4bca2034de78ac9ec68597cb28b0c0e
bayan_kassem <bayank@wix.com>
2025-12-02 09:28:00 +0200

    removed async

--- pages/boardPeoplePage.js
+++ pages/boardPeoplePage.js
@@ -6,14 +6,14 @@ async function boardPeoplePageOnReady(_$w,) {
 }
 
 async function bindBoardPeopleRepeaters(_$w) {
-    _$w('#directorsRepeaterItem').onClick(async (event) => {
+    _$w('#directorsRepeaterItem').onClick((event) => {
         const $item = _$w.at(event.context);
         const clickedItemData = $item('#dynamicDataset').getCurrentItem();
         location.to(`${clickedItemData['link-board-people-title_fld']}`);
-        
+
     });
 
-    _$w('#executivesRepeaterItem').onClick(async (event) => {
+    _$w('#executivesRepeaterItem').onClick((event) => {
         const $item = _$w.at(event.context);
         const clickedItemData = $item('#dataset1').getCurrentItem();
         location.to(`${clickedItemData['link-board-people-title_fld']}`);
