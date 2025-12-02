c926dd2f3927e6431ce14d5e9aef5e25466a0756
bayan_kassem <bayank@wix.com>
2025-12-02 08:59:36 +0200

    added debug logs

--- pages/boardPeoplePage.js
+++ pages/boardPeoplePage.js
@@ -2,14 +2,16 @@
 const { location } = require("@wix/site-location");
 
 async function boardPeoplePageOnReady(_$w,) {
+    console.log("boardPeoplePageOnReady");
     await bindBoardPeopleRepeaters(_$w);
 }
 
 async function bindBoardPeopleRepeaters(_$w) {
-
+    console.log("bindBoardPeopleRepeaters");
     _$w('#directorsRepeaterItem').onClick((event) => {
         const $item = _$w.at(event.context);
         const clickedItemData = $item('#dynamicDataset').getCurrentItem();
+        console.log("clickedItemData directors: ",clickedItemData);
         location.to(`/${clickedItemData['link-board-people-title_fld']}`);
 
     });
@@ -17,6 +19,7 @@ async function bindBoardPeopleRepeaters(_$w) {
     _$w('#executivesRepeaterItem').onClick((event) => {
         const $item = _$w.at(event.context);
         const clickedItemData = $item('#dataset1').getCurrentItem();
+        console.log("clickedItemData executives: ",clickedItemData);
         location.to(`/${clickedItemData['link-board-people-title_fld']}`);
 
     });
