0822e29349b01fb0cee76531b55563ea7fc122c0
bayan_kassem <bayank@wix.com>
2025-12-02 09:22:31 +0200

    removed debug logs and removed / from location to

--- pages/boardPeoplePage.js
+++ pages/boardPeoplePage.js
@@ -2,27 +2,20 @@
 const { location } = require("@wix/site-location");
 
 async function boardPeoplePageOnReady(_$w,) {
-    console.log("boardPeoplePageOnReady");
     await bindBoardPeopleRepeaters(_$w);
 }
 
 async function bindBoardPeopleRepeaters(_$w) {
-    console.log("bindBoardPeopleRepeaters");
     _$w('#directorsRepeaterItem').onClick(async (event) => {
         const $item = _$w.at(event.context);
         const clickedItemData = $item('#dynamicDataset').getCurrentItem();
-        console.log("clickedItemData directors: ",clickedItemData);
-        const baseUrl = await location.baseUrl();
-        console.log("base url: ",baseUrl);
         location.to(`${clickedItemData['link-board-people-title_fld']}`);
-        
     });
 
     _$w('#executivesRepeaterItem').onClick(async (event) => {
         const $item = _$w.at(event.context);
         const clickedItemData = $item('#dataset1').getCurrentItem();
-        console.log("clickedItemData executives: ",clickedItemData);
-        await location.to(`/${clickedItemData['link-board-people-title_fld']}`);
+        location.to(`${clickedItemData['link-board-people-title_fld']}`);
 
     });
 }
